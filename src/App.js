dce9d2ab92e8833249c335ea0e8380519a033915
rad-codes <67544116+rad-codes@users.noreply.github.com>
2022-09-17 18:30:40 +0530

    initial commit
    goodbooks react app

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React,{useState} from "react";
+import "./App.css";
+
+
+const bookDB = {
+  javascript: [
+    { name: "Eloquent JavaScript", rating: "4/5" },
+    { name: "You Don't Know JS", rating: "3.5/5" },
+    { name: "JavaScript: The Good Parts", rating: "4.5/5" },
+    { name: "JavaScript Data Structures and Algorithms: An Introduction to Understanding and Implementing Core Data Structure and Algorithm Fundamentals", rating: "4.3/5" }
+  
+  ],
+
+  fiction: [
+    {
+      name: "Shiva Trilogy",
+      rating: "5/5"
+    },
+    {
+      name: "Harry Potter and the Sorcerer's Stone",
+      rating: "4.5/5"
+    },
+    
+    {
+      name: "The Alchemist ",
+      rating: "4.3/5"
+    }
+  ],
+  business: [
+    {
+      name: "Never Split the Difference",
+      rating: "3.5/5"
+    },
+    {
+      name: "Loonshots",
+      rating: "5/5"
+    },
+    {
+      name: "Zero to One",
+      rating: "4/5"
+    }
+  ]
+};
+
+export default function App(){
+  const [selectedGenre, setGenre] = useState("fiction");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1> 📚 goodbooks </h1>
+      <p style={{ fontSize: "smaller" }}>
+        {" "}
+        Checkout my favorite books. Select a genre to get started{" "}
+      </p>
+
+      <div>
+        {Object.keys(bookDB).map((genre) => (
+          <button
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              borderRadius: "0.5rem",
+              padding: "1rem  1rem",
+              margin: "1rem 0.3rem"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {bookDB[selectedGenre].map((book) => (
+            <li
+              key={book.name}
+              style={{
+                // listStyle: "none",
+                padding: "1rem  1rem",
+                margin: "1rem 0.3rem",
+                border: "1px solid #d1d5db",
+                borderRadius: "0.5rem"
+              }}
+            >
+              {" "}
+              <div style={{ fontSize: "larger" }}> {book.name} </div>
+              <div style={{ fontSize: "smaller" }}> {book.rating} </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+            }
